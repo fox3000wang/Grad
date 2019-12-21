050da49654c878ea48a5c18775f7b3ecdaffcfd0
fox.wang <fox.wang@saybot.com>
2019-12-21 11:33:22 +0800

    add webpack

---
+++ build/build.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+'use strict';
+
+const fs = require('fs');
+const ora = require('ora');
+const rm = require('rimraf');
+const path = require('path');
+const chalk = require('chalk');
+const webpack = require('webpack');
+const config = require('../config');
+const webpackConfig = require('./webpack.conf');
